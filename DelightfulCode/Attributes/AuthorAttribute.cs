3c43d997ce936d432d527b7ff55beac063a4c471
Warren James <48476294+mousetrap-systems@users.noreply.github.com>
2023-05-09 19:12:20 +0100

    This allows extensions of 'Year' attribute ... you can now do multiple ranges
    Some help from GPT-4 to extend the usage.
    
    This allows identifying historical contributions when code spans over several years.

--- DelightfulCode/Attributes/AuthorAttribute.cs
+++ DelightfulCode/Attributes/AuthorAttribute.cs
@@ -6,25 +6,37 @@
 /// Similar to NUNIT https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/attributes
 /// </summary>
 [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
-[Author("Warren James", 2022)]
+[Author("GPT-4", 2023)]
+[Author("Warren James", 2022-2023)]
 [Health(CodeStability.Stable)]
 public sealed class AuthorAttribute : Attribute
 {
     private string name;
+    private int[] years;
 
     /// <summary>
     /// Indicates the (currently known) health of this portion of code.
+    /// You can specify multiple years, and also a range, for each year of contribution.
     /// </summary>
-    /// <param name="contributor_name">Who added to/was responsible for, this code logic?</param>
-    /// <param name="year">Optionally indicate the year written (helps identify old code quickly)</param>
-    public AuthorAttribute(string contributor_name, int year = 0)
+    /// <example>
+    /// this: [Author("Warren James", 2018-2021, 2023)]
+    /// </example>
+    /// <param name="contributor_name">Who added to/was responsible for, this code logic? To protect privacy, do not use email addresses.</param>
+    /// <param name="years">Optionally indicate the years of contribution (helps identify old code quickly)</param>
+    public AuthorAttribute(string contributor_name, params int[] years)
     {
         this.name = contributor_name;
+        this.years = years;
     }
 
     public string Name
     {
         get { return name; }
+    }
+
+    public int[] Years
+    {
+        get { return years; }
     }
 
 }
